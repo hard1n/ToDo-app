3082f462837842c146276e41918c4abc56841764
Hardin <hardisonpaulino@gmail.com>
2024-01-31 14:25:14 -0400

    [feat]: Layout improved

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+const $taskContainer = document.getElementById("tasks-container");
+const $taskWarning = document.getElementById("task-warning");
 const $template = document.getElementById("task-template");
+const $fragment = document.createDocumentFragment();
 
 document.addEventListener("submit", (e) => {
   e.preventDefault();
+  const $input = e.target.querySelector("input");
+  const $clone = $template.content.cloneNode(true);
 
-  console.log(e.target);
+  if ($input.value !== "") {
+    // console.log($input.value);
+    // $taskWarning.classList.add("ocultar");
+    console.log($taskWarning);
+    $clone.querySelector("p").textContent = $input.value;
+    $fragment.appendChild($clone);
+    $taskContainer.appendChild($fragment);
+    $input.value = "";
+  }
 });
